550b26073336d4a09455fe4547a496abde0d54e2
unknown <gui.cavalcante1999@outlook.com.br>
2023-06-05 13:49:36 -0300

    add: mapemando os relacionamentos

--- api/src/main/java/br/com/bytewizards/api/entity/DistribuicaoEntity.java
+++ api/src/main/java/br/com/bytewizards/api/entity/DistribuicaoEntity.java
@@ -19,17 +19,27 @@ public class DistribuicaoEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="ID_DISTRIBUICAO")
     private Long id;
 
     @Embedded
+    @Column(name="ENDERECO_DISTRIBUICAO")
     private EnderecoEntity endereco;
 
+    @Column(name="DATA_DISTRIBUICAO")
     private LocalDate data;
 
+    @Column(name="HORA_INICIO_DISTRIBUICAO")
     private LocalTime horaInicio;
 
+    @Column(name="HORA_FIM_DISTRIBUICAO")
     private LocalTime horaFim;
 
+    @Column(name="ATIVO")
     private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name="ID_ONG")
+    private OngEntity ong;
 
 }
