7dcab39c0960e522ada14824d09ca452a574abb3
unknown <gui.cavalcante1999@outlook.com.br>
2023-06-06 17:38:21 -0300

    fix: consertando o retorno da classe OngController

--- api/src/main/java/br/com/bytewizards/api/entity/EnderecoEntity.java
+++ api/src/main/java/br/com/bytewizards/api/entity/EnderecoEntity.java
@@ -1,5 +1,6 @@
 package br.com.bytewizards.api.entity;
 
+import br.com.bytewizards.api.entity.dto.EnderecoDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,4 +24,13 @@ public class EnderecoEntity {
 
     private String cidade;
 
+
+    public EnderecoEntity(EnderecoDto dados) {
+        this.logradouro = dados.logradouro();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cep = dados.uf();
+        this.uf = dados.uf();
+        this.cidade = dados.cidade();
+    }
 }
